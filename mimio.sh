eecbc4b0657bc6903b682ebbde92d063307b421e
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2024-12-01 00:35:36 +0900

    Update mimio.sh

--- mimio.sh
+++ mimio.sh
@@ -8,29 +8,21 @@ Documentation=https://min.io/docs/minio/linux/index.html
 Wants=network-online.target
 After=network-online.target
 AssertFileIsExecutable=/usr/local/bin/minio
-
 [Service]
 WorkingDirectory=/usr/local
-
 User=root
 Group=root
 ProtectProc=invisible
-
 EnvironmentFile=-/etc/default/minio
 ExecStart=/usr/local/bin/minio server --console-address :9001 /mnt/data
-
 Restart=always
-
 # Specifies the maximum file descriptor number that can be opened by this process
 LimitNOFILE=65536
-
 # Specifies the maximum number of threads this process can create
 TasksMax=infinity
-
 # Disable timeout logic and wait until process is stopped
 TimeoutStopSec=infinity
 SendSIGKILL=no
-
 [Install]
 WantedBy=multi-user.target
 EOF
